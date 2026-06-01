357d9774241798ebcab056fc289be187ac1c26eb
vika <vk.cbd.68@gmail.com>
2026-06-01 19:54:30 +0300

    Fix Selenium headless mode for CI

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,29 +1,31 @@
 const { Builder, By } = require('selenium-webdriver');
-const path = require('path');
+const chrome = require('selenium-webdriver/chrome');
 
 async function runTests() {
 
-    let driver = await new Builder().forBrowser('chrome').build();
+    let options = new chrome.Options();
+    options.addArguments('--headless=new');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
 
     try {
 
-        const filePath = 'file://' + path.resolve(__dirname, '../index.html');
+        await driver.get('file://' + __dirname + '/../index.html');
 
-        await driver.get(filePath);
-
-        // Тест 1
         let title = await driver.findElement(By.id('title'));
         console.log("Тест 1 пройден");
 
-        // Тест 2
         let button = await driver.findElement(By.id('submitBtn'));
         console.log("Тест 2 пройден");
 
-        // Тест 3
         let input = await driver.findElement(By.id('name'));
         console.log("Тест 3 пройден");
 
-        // Тест 4
         let text = await button.getText();
 
         if (text !== 'Отправить') {
